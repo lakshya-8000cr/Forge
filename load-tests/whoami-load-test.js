1927c244bde98db949cfc3e9567d645430b23bfe
lakshya-8000cr <lakshyachauhan147@gmail.com>
2026-07-05 17:48:49 +0530

    docs , load-testing

---
+++ load-tests/whoami-load-test.js
@@ -0,0 +1,19 @@
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+export const options = {
+  vus: 1000,
+  duration: "2m",
+};
+
+export default function () {
+  const BASE_URL = "http://a6228a9f5a97041acb1f84aa6ada2478-774918050.eu-north-1.elb.amazonaws.com";
+  
+  const res = http.get(`${BASE_URL}/apps/whoami-demo`);
+
+  check(res, {
+    "status is 200": (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
