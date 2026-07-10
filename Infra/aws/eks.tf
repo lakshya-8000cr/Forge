c0dfe90b23b787b43d15258eef99b688dce252d8
lakshya-8000cr <lakshyachauhan147@gmail.com>
2026-07-10 09:38:38 +0530

    back to normal

--- Infra/aws/eks.tf
+++ Infra/aws/eks.tf
@@ -62,3 +62,29 @@ resource "aws_eks_node_group" "forge_nodes" {
     Name = "forge-workers"
   }
 }
+resource "aws_instance" "forge_k6_runner" {
+  ami           = "ami-07a0715df72e58928"
+  instance_type = "t3.small"
+  subnet_id     = aws_subnet.forge_public_subnet.id
+
+  vpc_security_group_ids = [
+    aws_security_group.forge_ec2_sg.id
+  ]
+
+  # 🔑 NAYI KEY YAHAN ADD KARO (Ensure properties reflect this exact name)
+  key_name = "forge-key-v2" 
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install gnupg software-properties-common curl -y
+              curl -s https://dl.k6.io/key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/k6-archive-keyring.gpg
+              echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
+              sudo apt update -y
+              sudo apt install k6 -y
+              EOF
+
+  tags = {
+    Name = "forge-test-ec2"
+  }
+}
